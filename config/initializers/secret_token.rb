00fe6284a1b6bda8105caed0f6b30bee2fefb0c1
Jake Sikora <jakesikora.01@gmail.com>
2013-11-22 22:46:08 -0600

    new app with PG/rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HearthstoneScores::Application.config.secret_key_base = 'bfddab7b3c06fe32bf2f60cfc790736419f4f8848028e5b544b8cc37ff2190d90e515af39773d4c1ccbedc2ed535de2daf1630f61ebb8161fac403ec1fc38c03'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HearthstoneScores::Application.config.session_store :cookie_store, key: '_hearthstone_scores_session'
