9c65888ad9728598e78461c9a2aab76f39ba3554
Jake Sikora <jakesikora.01@gmail.com>
2013-11-22 23:37:32 -0600

    username and email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :username, presence: true,
+            length: {maximum: 16}
+
+  validates :email, presence: true,
+            format: {with: /\w+@\w+\.\w{2,}/}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,37 @@ describe User do
   it 'should create a new instance given valid attributes' do
     expect { User.create!(@attr) }.to change(User, :count).by(1)
   end
+
+  describe 'Username Validations' do
+    it 'should require username' do
+      un_named_user = User.new(@attr.merge(username: ''))
+      un_named_user.should_not be_valid
+    end
+
+    it 'should prevent username from being too long' do
+      long_name = 'a'*17
+      
+      user_with_long_name = User.new(@attr.merge(username: long_name))
+      user_with_long_name.should_not be_valid
+    end
+  end
+
+  describe 'Email Validations' do
+    it 'should reject improper format' do
+      invalid_emails = %w(foo@bar foo_bar.buz foo@bar.b)
+      invalid_emails.each do |email|
+        user_invalid_email = User.new(@attr.merge(email: email))
+        user_invalid_email.should_not be_valid
+      end
+    end
+
+    it 'should accept proper format' do
+      valid_emails = %w(foo@bar.com foo-bar@bazz.buz)
+      valid_emails.each do |email|
+        user_valid_email = User.new(@attr.merge(email: email))
+        user_valid_email.should be_valid
+      end
+    end
+
+  end
 end
